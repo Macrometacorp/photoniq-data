16aa15c71c2347528c69dd6ca0654a0cee25683b
Joubert Berger <joubert.berger@macrometa.com>
2023-11-28 10:53:15 +0100

    Rework how we get list of items to download

--- scripts/download.sh
+++ scripts/download.sh
@@ -1,4 +1,4 @@
-#!/bin/bash
+#!/bin/bash 
 BASE="$GITHUB_WORKSPACE"
 # Function to download a file if it has been updated
 download_file() {
@@ -8,13 +8,13 @@ download_file() {
     local headers_file="headers.txt"
 
     # Check if the ETag file exists and read the ETag
-    local etag=""
+    etag="000"
     if [ -f "$etag_file" ]; then
-        etag=$(cat "$etag_file")
+        etag=$(cat $etag_file)
     fi
 
-    # Use 'curl' to download the file and headers. '-D' dumps headers to a file.
-    curl -s -D "$headers_file" -H "If-None-Match: $etag" "$url" -o "$download_file"
+    # Use 'curl' to download the file and headers. 
+    curl -s -D $headers_file -H "If-None-Match: $etag" $url -o $download_file
 
     # Check HTTP status code from the headers file
     local http_status=$(grep "^HTTP/[0-9]" "$headers_file" | awk '{print $2}')
@@ -22,17 +22,28 @@ download_file() {
     # Check if the file was modified and downloaded (HTTP 200)
     if [ "$http_status" = "200" ]; then
         # Extract and store the new ETag from the headers file
-        local new_etag=$(grep -i ETag "$headers_file" | awk '{print $2}')
-        echo "$new_etag" > "$etag_file"
+        local new_etag=$(grep -i ETag "$headers_file" | awk '{print $2}'| sed 's/\r//')
+        echo $new_etag > "$etag_file"
     fi
 
     # Clean up: Remove the headers file
-    rm "$headers_file"
+    rm -f $headers_file
 }
 
-#"https://github.com/BryanMorgan/isbot/blob/main/src/bot_regex_patterns.txt"
+split_element() {
+    echo "$1" | tr '|' ' ' | while read -r part; do
+    	echo "$part"
+    done
+}
 
-download_file "https://raw.githubusercontent.com/BryanMorgan/isbot/main/src/bot_regex_patterns.txt" "bot_regex_patterns.etag" bot_regex_patterns.txt
+download_items=$DOWNLOAD_ITEMS
 
-download_file "https://iplists.firehol.org/files/firehol_level1.netset" firehol_level1.etag ipv4.blacklist
+
+for element in $download_items; do
+    translated_sep=$(echo "$element" | sed 's/%7C/|/g'| sed 's/"//g')
+    # Capture the output of split_element into a variable
+    parts=$(split_element $translated_sep)
+
+    download_file $parts
+done
 
